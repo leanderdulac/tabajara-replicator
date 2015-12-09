11b2e6ec5341516bd0b0cda5115f0567faf1ae9f
Pedro Franceschi <pedrohfranceschi@gmail.com>
2015-12-09 13:47:43 -0800

    Initialize Kodama with all settings.

--- lib/tabajara_replicator/replicator.rb
+++ lib/tabajara_replicator/replicator.rb
@@ -8,7 +8,11 @@ module TabajaraReplicator
 
 			Kodama::Client.start(@app.config[:mysql]) do |c|
 				puts "Listening to #{@app.config[:mysql][:host]}:#{@app.config[:mysql][:port]}"
+
 				c.binlog_position_file = @app.config[:binlog_position_file]
+				c.log_level = @app.config[:log_level]
+				c.connection_retry_limit = @app.config[:connection_retry_limit]
+				c.connection_retry_wait = @app.config[:connection_retry_wait]
 
 				c.on_row_event do |event|
 					perform(event)
